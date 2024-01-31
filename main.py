8be588ab78d69f5d9fe746714d0d1da55b2e53ed
Rupsa Chatterjee <rupsa.chatterjee@synergicsoftek.in>
2024-01-31 18:55:08 +0530

    port change 31/1/24

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ origins = [
 ]
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=3003, reload=True)
 
 app.add_middleware(
     CORSMiddleware,
