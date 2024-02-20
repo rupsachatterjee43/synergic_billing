d9c5aef65dc7582b06205cc1eba1596fa31da218
Rupsa Chatterjee <rupsa.chatterjee@synergicsoftek.in>
2024-02-20 10:32:10 +0530

    version

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ origins = [
 ]
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=3003, reload=True)
+   uvicorn.run("main:app", host="0.0.0.0", port=3004, reload=True)
 
 app.add_middleware(
     CORSMiddleware,
